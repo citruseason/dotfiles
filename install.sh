cd8ded52aad71d4acb26fdbfb5297ecd49ecfa61
Kim YoungJung <jigm1539@gmail.com>
2016-10-11 00:01:29 +0900

    mac 기본설정 추가

--- install.sh
+++ install.sh
@@ -228,3 +228,343 @@ cp ./plist/com.googlecode.iterm2.plist ~/Library/Preferences
 defaults read com.googlecode.iterm2.plist
 defaults read -app iTerm > /dev/null 2>&1;
 ok
+
+
+###############################################################################
+bot "Trackpad, mouse, keyboard, Bluetooth accessories, and input"
+###############################################################################
+
+running "Trackpad: enable tap to click for this user and for the login screen"
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1;ok
+
+running "Enable tap to click. (Don't have to press down on the trackpad -- just tap it.)"
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+defaults write com.apple.AppleMultitouchTrackpad Clicking -bool true
+
+running "Enable 3-finger drag. (Moving with 3 fingers in any window chrome moves the window.)"
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadThreeFingerDrag -bool true
+defaults write com.apple.AppleMultitouchTrackpad TrackpadThreeFingerDrag -bool true
+
+running "Enable 'natural' (Lion-style) scrolling"
+defaults write NSGlobalDomain com.apple.swipescrolldirection -bool true;ok
+
+running "Increase sound quality for Bluetooth headphones/headsets"
+defaults write com.apple.BluetoothAudioAgent "Apple Bitpool Min (editable)" -int 40;ok
+
+running "Enable press-and-hold for keys in favor of key repeat"
+defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false;ok
+
+running "Set a blazingly fast keyboard repeat rate"
+defaults write NSGlobalDomain KeyRepeat -int 0;
+
+running "Disable auto-correct"
+defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false;ok
+
+
+###############################################################################
+bot "Configuring the Screen"
+###############################################################################
+
+running "Save screenshots to the desktop"
+defaults write com.apple.screencapture location -string "${HOME}/Desktop";ok
+
+running "Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)"
+defaults write com.apple.screencapture type -string "png";ok
+
+running "Disable shadow in screenshots"
+defaults write com.apple.screencapture disable-shadow -bool true;ok
+
+running "Enable subpixel font rendering on non-Apple LCDs"
+defaults write NSGlobalDomain AppleFontSmoothing -int 2;ok
+
+running "Require password immediately after sleep or screen saver begins"
+defaults write com.apple.screensaver askForPassword -int 1
+defaults write com.apple.screensaver askForPasswordDelay -int 0;ok
+
+
+###############################################################################
+bot "Finder Configs"
+###############################################################################
+
+running "Disable window animations and Get Info animations"
+defaults write com.apple.finder DisableAllAnimations -bool true;ok
+
+running "Show hidden files by default"
+defaults write com.apple.finder AppleShowAllFiles -bool true;ok
+
+running "Show all filename extensions"
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true;ok
+
+running "Show status bar"
+defaults write com.apple.finder ShowStatusBar -bool true;ok
+
+running "Show path bar"
+defaults write com.apple.finder ShowPathbar -bool true;ok
+
+running "Allow text selection in Quick Look"
+defaults write com.apple.finder QLEnableTextSelection -bool true;ok
+
+running "Display full POSIX path as Finder window title"
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool true;ok
+
+running "When performing a search, search the current folder by default"
+defaults write com.apple.finder FXDefaultSearchScope -string "SCcf";ok
+
+running "Disable the warning when changing a file extension"
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false;ok
+
+running "Enable spring loading for directories"
+defaults write NSGlobalDomain com.apple.springing.enabled -bool true;ok
+
+running "Remove the spring loading delay for directories"
+defaults write NSGlobalDomain com.apple.springing.delay -float 0;ok
+
+running "Avoid creating .DS_Store files on network volumes"
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true;ok
+
+running "Disable disk image verification"
+defaults write com.apple.frameworks.diskimages skip-verify -bool true
+defaults write com.apple.frameworks.diskimages skip-verify-locked -bool true
+defaults write com.apple.frameworks.diskimages skip-verify-remote -bool true;ok
+
+running "Automatically open a new Finder window when a volume is mounted"
+defaults write com.apple.frameworks.diskimages auto-open-ro-root -bool true
+defaults write com.apple.frameworks.diskimages auto-open-rw-root -bool true
+defaults write com.apple.finder OpenWindowForNewRemovableDisk -bool true;ok
+
+running "Use list view in all Finder windows by default"
+# Four-letter codes for the other view modes: `icnv`, `clmv`, `Flwv`
+defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv";ok
+
+running "Disable the warning before emptying the Trash"
+defaults write com.apple.finder WarnOnEmptyTrash -bool false;ok
+
+running "Empty Trash securely by default"
+defaults write com.apple.finder EmptyTrashSecurely -bool true;ok
+
+running "Enable AirDrop over Ethernet and on unsupported Macs running Lion"
+defaults write com.apple.NetworkBrowser BrowseAllInterfaces -bool true;ok
+
+running "Show the ~/Library folder"
+chflags nohidden ~/Library;ok
+
+running "Expand the following File Info panes: “General”, “Open with”, and “Sharing & Permissions”"
+defaults write com.apple.finder FXInfoPanesExpanded -dict \
+    General -bool true \
+    OpenWith -bool true \
+    Privileges -bool true;ok
+
+
+###############################################################################
+bot "Dock & Dashboard"
+###############################################################################
+
+running "Enable highlight hover effect for the grid view of a stack (Dock)"
+defaults write com.apple.dock mouse-over-hilite-stack -bool true;ok
+
+running "Set the icon size of Dock items to 36 pixels"
+defaults write com.apple.dock tilesize -int 36;ok
+
+running "Change minimize/maximize window effect to scale"
+defaults write com.apple.dock mineffect -string "scale";ok
+
+running "Minimize windows into their application’s icon"
+defaults write com.apple.dock minimize-to-application -bool true;ok
+
+running "Enable spring loading for all Dock items"
+defaults write com.apple.dock enable-spring-load-actions-on-all-items -bool true;ok
+
+running "Show indicator lights for open applications in the Dock"
+defaults write com.apple.dock show-process-indicators -bool true;ok
+
+running "Don’t animate opening applications from the Dock"
+defaults write com.apple.dock launchanim -bool false;ok
+
+running "Speed up Mission Control animations"
+defaults write com.apple.dock expose-animation-duration -float 0.1;ok
+
+running "Don’t group windows by application in Mission Control"
+# (i.e. use the old Exposé behavior instead)
+defaults write com.apple.dock expose-group-by-app -bool false;ok
+
+running "Disable Dashboard"
+defaults write com.apple.dashboard mcx-disabled -bool true;ok
+
+running "Don’t show Dashboard as a Space"
+defaults write com.apple.dock dashboard-in-overlay -bool true;ok
+
+running "Don’t automatically rearrange Spaces based on most recent use"
+defaults write com.apple.dock mru-spaces -bool false;ok
+
+running "Remove the auto-hiding Dock delay"
+defaults write com.apple.dock autohide-delay -float 0;ok
+
+running "Remove the animation when hiding/showing the Dock"
+defaults write com.apple.dock autohide-time-modifier -float 0;ok
+
+running "Automatically hide and show the Dock"
+defaults write com.apple.dock autohide -bool true;ok
+
+running "Make Dock icons of hidden applications translucent"
+defaults write com.apple.dock showhidden -bool true;ok
+
+running "Make Dock more transparent"
+defaults write com.apple.dock hide-mirror -bool true;ok
+
+running "Reset Launchpad, but keep the desktop wallpaper intact"
+find "${HOME}/Library/Application Support/Dock" -name "*-*.db" -maxdepth 1 -delete;ok
+
+bot "Configuring Hot Corners"
+# Possible values:
+#  0: no-op
+#  2: Mission Control
+#  3: Show application windows
+#  4: Desktop
+#  5: Start screen saver
+#  6: Disable screen saver
+#  7: Dashboard
+# 10: Put display to sleep
+# 11: Launchpad
+# 12: Notification Center
+
+running "Top left screen corner → Mission Control"
+defaults write com.apple.dock wvous-tl-corner -int 0
+defaults write com.apple.dock wvous-tl-modifier -int 0;ok
+running "Top right screen corner → Desktop"
+defaults write com.apple.dock wvous-tr-corner -int 0
+defaults write com.apple.dock wvous-tr-modifier -int 0;ok
+running "Bottom right screen corner → Start screen saver"
+defaults write com.apple.dock wvous-br-corner -int 0
+defaults write com.apple.dock wvous-br-modifier -int 0;ok
+
+
+###############################################################################
+bot "Configuring Safari & WebKit"
+###############################################################################
+
+running "Set Safari’s home page to ‘about:blank’ for faster loading"
+defaults write com.apple.Safari HomePage -string "about:blank";ok
+
+running "Prevent Safari from opening ‘safe’ files automatically after downloading"
+defaults write com.apple.Safari AutoOpenSafeDownloads -bool false;ok
+
+running "Allow hitting the Backspace key to go to the previous page in history"
+defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2BackspaceKeyNavigationEnabled -bool
+true;ok
+
+running "Hide Safari’s bookmarks bar by default"
+defaults write com.apple.Safari ShowFavoritesBar -bool false;ok
+
+running "Hide Safari’s sidebar in Top Sites"
+defaults write com.apple.Safari ShowSidebarInTopSites -bool false;ok
+
+running "Disable Safari’s thumbnail cache for History and Top Sites"
+defaults write com.apple.Safari DebugSnapshotsUpdatePolicy -int 2;ok
+
+running "Enable Safari’s debug menu"
+defaults write com.apple.Safari IncludeInternalDebugMenu -bool true;ok
+
+running "Make Safari’s search banners default to Contains instead of Starts With"
+defaults write com.apple.Safari FindOnPageMatchesWordStartsOnly -bool false;ok
+
+running "Remove useless icons from Safari’s bookmarks bar"
+defaults write com.apple.Safari ProxiesInBookmarksBar "()";ok
+
+running "Enable the Develop menu and the Web Inspector in Safari"
+defaults write com.apple.Safari IncludeDevelopMenu -bool true
+defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
+defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled -bool true;ok
+
+running "Add a context menu item for showing the Web Inspector in web views"
+defaults write NSGlobalDomain WebKitDeveloperExtras -bool true;ok
+
+
+###############################################################################
+bot "Configuring Mail"
+###############################################################################
+
+running "Disable send and reply animations in Mail.app"
+defaults write com.apple.mail DisableReplyAnimations -bool true
+defaults write com.apple.mail DisableSendAnimations -bool true;ok
+
+running "Copy email addresses as 'foo@example.com' instead of 'Foo Bar <foo@example.com>' in Mail.app"
+defaults write com.apple.mail AddressesIncludeNameOnPasteboard -bool false;ok
+
+running "Add the keyboard shortcut ⌘ + Enter to send an email in Mail.app"
+defaults write com.apple.mail NSUserKeyEquivalents -dict-add "Send" -string "@\\U21a9";ok
+
+running "Display emails in threaded mode, sorted by date (oldest at the top)"
+defaults write com.apple.mail DraftsViewerAttributes -dict-add "DisplayInThreadedMode" -string "yes"
+defaults write com.apple.mail DraftsViewerAttributes -dict-add "SortedDescending" -string "yes"
+defaults write com.apple.mail DraftsViewerAttributes -dict-add "SortOrder" -string "received-date";ok
+
+running "Disable inline attachments (just show the icons)"
+defaults write com.apple.mail DisableInlineAttachmentViewing -bool true;ok
+
+running "Disable automatic spell checking"
+defaults write com.apple.mail SpellCheckingBehavior -string "NoSpellCheckingEnabled";ok
+
+
+###############################################################################
+bot "Time Machine"
+###############################################################################
+
+running "Prevent Time Machine from prompting to use new hard drives as backup volume"
+defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true;ok
+
+running "Disable local Time Machine backups"
+hash tmutil &> /dev/null && sudo tmutil disablelocal;ok
+
+
+###############################################################################
+bot "Activity Monitor"
+###############################################################################
+
+running "Show the main window when launching Activity Monitor"
+defaults write com.apple.ActivityMonitor OpenMainWindow -bool true;ok
+
+running "Visualize CPU usage in the Activity Monitor Dock icon"
+defaults write com.apple.ActivityMonitor IconType -int 5;ok
+
+running "Show all processes in Activity Monitor"
+defaults write com.apple.ActivityMonitor ShowCategory -int 0;ok
+
+running "Sort Activity Monitor results by CPU usage"
+defaults write com.apple.ActivityMonitor SortColumn -string "CPUUsage"
+defaults write com.apple.ActivityMonitor SortDirection -int 0;ok
+
+
+###############################################################################
+bot "Address Book, Dashboard, iCal, TextEdit, and Disk Utility"
+###############################################################################
+
+running "Enable the debug menu in Address Book"
+defaults write com.apple.addressbook ABShowDebugMenu -bool true;ok
+
+running "Enable Dashboard dev mode (allows keeping widgets on the desktop)"
+defaults write com.apple.dashboard devmode -bool true;ok
+
+running "Use plain text mode for new TextEdit documents"
+defaults write com.apple.TextEdit RichText -int 0;ok
+running "Open and save files as UTF-8 in TextEdit"
+defaults write com.apple.TextEdit PlainTextEncoding -int 4
+defaults write com.apple.TextEdit PlainTextEncodingForWrite -int 4;ok
+
+running "Enable the debug menu in Disk Utility"
+defaults write com.apple.DiskUtility DUDebugMenuEnabled -bool true
+defaults write com.apple.DiskUtility advanced-image-options -bool true;ok
+
+
+###############################################################################
+# Kill affected applications                                                  #
+# ###############################################################################
+bot "OK. Note that some of these changes require a logout/restart to take effect. Killing affected applications (so they can reboot)...."
+for app in "Activity Monitor" "Address Book" "Calendar" "Contacts" "cfprefsd" \
+    "Dock" "Finder" "Mail" "Messages" "Safari" "SizeUp" "SystemUIServer" \
+    "iCal" "Terminal"; do
+    killall "${app}" > /dev/null 2>&1
+done
+
+bot "Woot! All done. Kill this terminal and launch iTerm"
